6d1be679d7a0e85299568f97324c94eb9186bf0e
tarao <tarao.gnn@gmail.com>
2018-12-12 09:55:14 +0900

    Output result JSON and exit successfully on CI.

---
+++ verifier/ci.sh
@@ -0,0 +1,11 @@
+on_exit() {
+    [ "$imports_ok" = 1 ] || {
+        json='{"success":false,"score":0,"steps":0,"used_switches":0,"message":"Violation: detected an illegal import line"}'
+    }
+    echo "verifier.result:$json"
+    exit
+}
+
+[ "$CI" = 'true' ] && {
+    trap on_exit EXIT
+} || true
