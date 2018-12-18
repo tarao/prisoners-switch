6a2cc446633a97e228035e2f536fac30142cc3c0
funa584 <1231aaaaaaaaaaaaaaaaaaaaaaaaaa@gmail.com>
2018-12-19 00:46:17 +0900

    test

--- strategy/my_strategy.go
+++ strategy/my_strategy.go
@@ -1,6 +1,8 @@
 package strategy
 
-import "github.com/tarao/prisoners-switch/rule"
+import (
+	"github.com/tarao/prisoners-switch/rule"
+)
 
 // MyNewStrategy returns a new strategy
 func MyNewStrategy() rule.Strategy {
@@ -15,10 +17,41 @@ func (s *myStrategy) NewPrisoner(number int, shout chan rule.Shout) rule.Prisone
 }
 
 type prisoner struct {
-	shout chan rule.Shout
+	shout              chan rule.Shout
+	defaultSwitchCount int
+	toggled            bool
 }
 
 func (p *prisoner) Enter(room rule.Room) {
 	// shout triumph when you think it's ready
-	p.shout <- rule.Triumph
+
+	aState := room.TakeSwitchA().State()
+	bState := room.TakeSwitchB().State()
+
+	if !p.toggled {
+		incrementSwitch(room)
+		p.toggled = true
+	}
+
+	if !aState && !bState {
+		p.defaultSwitchCount++
+	} else {
+		p.defaultSwitchCount = 0
+	}
+
+	if p.defaultSwitchCount > 1200 {
+		p.shout <- rule.Triumph
+	}
+}
+
+func incrementSwitch(room rule.Room) {
+	bState := room.TakeSwitchB().State()
+
+	if !bState {
+		room.TakeSwitchB().Toggle()
+		return
+	}
+
+	room.TakeSwitchA().Toggle()
+	room.TakeSwitchB().Toggle()
 }
