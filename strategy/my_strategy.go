6def2fceac14568b992827139dbf6850b49b5aa8
keijak <m@keijak.com>
2018-12-18 18:25:53 +0100

    a simple third-switching strategy (k=2000)

--- strategy/my_strategy.go
+++ strategy/my_strategy.go
@@ -1,6 +1,8 @@
 package strategy
 
 import "github.com/tarao/prisoners-switch/rule"
+	
+const kCount = 2000
 
 // MyNewStrategy returns a new strategy
 func MyNewStrategy() rule.Strategy {
@@ -16,9 +18,36 @@ func (s *myStrategy) NewPrisoner(number int, shout chan rule.Shout) rule.Prisone
 
 type prisoner struct {
 	shout chan rule.Shout
+	cnt int
+	done bool
+	initA bool
+	initB bool
 }
 
 func (p *prisoner) Enter(room rule.Room) {
-	// shout triumph when you think it's ready
+	if p.done {
+		return
+	}
+	if p.cnt == 0 {
+		p.initA = room.TakeSwitchA().State()
+		p.initB = room.TakeSwitchB().State()
+	}
+	p.cnt++
+	if p.cnt < kCount {
+		return
+	}
+	if p.initA == room.TakeSwitchA().State() && p.initB == room.TakeSwitchB().State() {
+		// probably the first
+		room.TakeSwitchA().Toggle()
+		p.done = true
+		return
+	}
+	if p.initB == room.TakeSwitchB().State() {
+		// probably the second
+		room.TakeSwitchB().Toggle()
+		p.done = true
+		return
+	}
+	// probably the third
 	p.shout <- rule.Triumph
 }
