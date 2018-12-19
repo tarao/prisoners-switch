ec514841c6848baae077592187a18fc0006547f8
keijak <m@keijak.com>
2018-12-19 12:21:42 +0100

    collect up to 3 counts (fix)

--- strategy/my_strategy.go
+++ strategy/my_strategy.go
@@ -1,8 +1,6 @@
 package strategy
 
 import "github.com/tarao/prisoners-switch/rule"
-	
-const kCount = 2000
 
 // MyNewStrategy returns a new strategy
 func MyNewStrategy() rule.Strategy {
@@ -13,41 +11,94 @@ type myStrategy struct {
 }
 
 func (s *myStrategy) NewPrisoner(number int, shout chan rule.Shout) rule.Prisoner {
-	return &prisoner{shout: shout}
+	return &prisoner{
+		shout:       shout,
+		isCollector: number == 0,
+		remaining:   rule.TotalPrisoners - 1, // number of workers not reporting yet
+	}
 }
 
 type prisoner struct {
-	shout chan rule.Shout
-	cnt int
-	done bool
-	initA bool
-	initB bool
+	shout       chan rule.Shout
+	isCollector bool
+	initialized bool
+
+	// collector fields.
+	remaining   int
+
+	// worker fields.
+	lastSeen    int
+	incremented bool
 }
 
 func (p *prisoner) Enter(room rule.Room) {
-	if p.done {
+	if p.isCollector {
+		p.collectorEnter(room)
+	} else {
+		p.workerEnter(room)
+	}
+}
+
+func (p *prisoner) workerEnter(room rule.Room) {
+	c := getCounter(room)
+	if !p.initialized {
+		p.lastSeen = c
+		p.initialized = true
 		return
 	}
-	if p.cnt == 0 {
-		p.initA = room.TakeSwitchA().State()
-		p.initB = room.TakeSwitchB().State()
-	}
-	p.cnt++
-	if p.cnt < kCount {
+	if p.lastSeen == c {
 		return
 	}
-	if p.initA == room.TakeSwitchA().State() && p.initB == room.TakeSwitchB().State() {
-		// probably the first
-		room.TakeSwitchA().Toggle()
-		p.done = true
+
+	if !p.incremented && c < 2 {
+		c++
+		setCounter(room, c)
+		p.incremented = true
+	}
+}
+
+func (p *prisoner) collectorEnter(room rule.Room) {
+	c := getCounter(room)
+	defer func(){
+		if c == 0 {
+			// change switch state to notify workers that the collector is ready.
+			setCounter(room, 1)
+			p.remaining++
+		} else {
+			setCounter(room, 0)
+		}
+	}()
+
+	if !p.initialized {
+		p.initialized = true
 		return
 	}
-	if p.initB == room.TakeSwitchB().State() {
-		// probably the second
-		room.TakeSwitchB().Toggle()
-		p.done = true
-		return
+	p.remaining -= c
+	if p.remaining == 0 {
+		p.shout <- rule.Triumph
 	}
-	// probably the third
-	p.shout <- rule.Triumph
+}
+
+func getCounter(room rule.Room) int {
+	sa, sb := room.TakeSwitchA(), room.TakeSwitchB()
+	c := 0
+	if sa.State() {
+		c += 1
+	}
+	if sb.State() {
+		c += 2
+	}
+	return c
+}
+
+func setCounter(room rule.Room, n int) {
+	sa, sb := room.TakeSwitchA(), room.TakeSwitchB()
+	a := (n % 2) != 0
+	b := (n / 2) != 0
+	if sa.State() != a {
+		sa.Toggle()
+	}
+	if sb.State() != b {
+		sb.Toggle()
+	}
 }
